354f61d518e43e973a2b723c5d7e7efbb920a5a2
Erin Mettille <ecmettille@gmail.com>
2020-04-22 18:44:25 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -3,7 +3,12 @@ class School
   roster = {}
   
   def add_student (name, grade)
-    roster[grade] = []
-    roster[grade] << name
+    if roster.include?(grade)
+      roster[grade] << name
+    else 
+      roster[grade] = []
+      roster[grade] << name
+    end
   end
+  
 end
