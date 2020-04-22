a0b65912d651ad91a73899a1e429c0edc685bc75
Erin Mettille <ecmettille@gmail.com>
2020-04-22 18:50:35 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,5 +1,15 @@
 class School 
+  attr_accessor :school
   
-  ROSTER = Hash.new
+  ROSTER = {}
+  
+  def add_student (name, grade)
+    if ROSTER.include?(grade)
+      ROSTER[grade] << name
+    else 
+      ROSTER[grade] = []
+      ROSTER[grade] << name
+    end
+  end
   
 end
